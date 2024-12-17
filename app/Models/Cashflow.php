4f8ded3b0ff4f8ea6d51e70fd9d630e5d67fd70f
mindraseptiawan <draktul007@gmail.com>
2024-12-18 06:26:57 +0700

    Update

--- app/Models/Cashflow.php
+++ app/Models/Cashflow.php
@@ -3,25 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
 
 class Cashflow extends Model
 {
-    use LogsActivity;
-
-    protected static $logAttributes = ['name', 'email'];
-
-    protected static $logName = 'user_activity';
-
-    public function getActivitylogOptions()
-    {
-        return LogOptions::defaults()
-            ->logOnly(['name', 'email']) // Mencatat hanya atribut tertentu
-            ->setDescriptionForEvent(fn(string $eventName) => "User {$eventName}");
-    }
     protected $table = 'cashflows';
 
+    // Specify the attributes that are mass assignable
     protected $fillable = [
         'transaction_id',
         'type',
--- app/Models/Kandang.php
+++ app/Models/Kandang.php
@@ -6,17 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Activitylog\Traits\LogsActivity;
-use Spatie\Activitylog\LogOptions;
 
 class Kandang extends Model
 {
     use HasFactory, SoftDeletes;
-    use LogsActivity;
 
-    protected static $logAttributes = ['name', 'email'];
 
-    protected static $logName = 'user_activity';
     /**
      * The attributes that are mass assignable.
      *
@@ -35,11 +30,5 @@ class Kandang extends Model
     public function pemeliharaans()
     {
         return $this->hasMany(Pemeliharaan::class, 'kandang_id', 'id');
-    }
-    public function getActivitylogOptions()
-    {
-        return LogOptions::defaults()
-            ->logOnly(['name', 'email']) // Mencatat hanya atribut tertentu
-            ->setDescriptionForEvent(fn(string $eventName) => "User {$eventName}");
     }
 }
