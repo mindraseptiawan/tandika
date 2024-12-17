25f5d38bd8be212f08b9465cb84b14f2428b696d
mindraseptiawan <draktul007@gmail.com>
2024-12-18 06:20:30 +0700

    Update

--- app/Models/Cashflow.php
+++ app/Models/Cashflow.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Cashflow extends Model
 {
+    use LogsActivity; // Mengaktifkan pencatatan aktivitas
+
+    // Tentukan atribut yang akan dicatat
+    protected static $logAttributes = ['name', 'email'];
+
+    // Nama log (Opsional)
+    protected static $logName = 'user_activity';
+
+    // Konfigurasi lebih lanjut untuk deskripsi log
+    public function getActivitylogOptions()
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'email']) // Mencatat hanya atribut tertentu
+            ->setDescriptionForEvent(fn(string $eventName) => "User {$eventName}");
+    }
     protected $table = 'cashflows';
 
     // Specify the attributes that are mass assignable
--- app/Models/Kandang.php
+++ app/Models/Kandang.php
@@ -6,11 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Kandang extends Model
 {
     use HasFactory, SoftDeletes;
+    use LogsActivity;
 
+    protected static $logAttributes = ['name', 'email'];
+
+    protected static $logName = 'user_activity';
+
+    public function getActivitylogOptions()
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'email']) // Mencatat hanya atribut tertentu
+            ->setDescriptionForEvent(fn(string $eventName) => "User {$eventName}");
+    }
 
     /**
      * The attributes that are mass assignable.
